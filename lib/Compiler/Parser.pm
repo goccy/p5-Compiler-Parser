82ddb520424e979f9fa586909eef4adbb6fe6f81
Masaaki Goshima <goccy54@gmail.com>
2013-09-10 18:27:27 +0900

    Checking in changes prior to tagging of version 0.04.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index eaf4c17..e55683a 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension Compiler-Parser.
    
     {{$NEXT}}
    
    +0.04 2013-09-10T09:27:11Z
    +
    +    - add Exception Handler
    +    - add some test codes
    +
     0.03 2013-09-07T14:45:06Z
    
         - add Module symbol.

--- lib/Compiler/Parser.pm
+++ lib/Compiler/Parser.pm
@@ -31,7 +31,7 @@ our @ISA = qw(Exporter);
 our %EXPORT_TAGS = ( 'all' => [ qw() ] );
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw();
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 require XSLoader;
 XSLoader::load('Compiler::Parser', $VERSION);
 
