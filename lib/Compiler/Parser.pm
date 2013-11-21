cd011b31a77824e6322472c8726a42a94b567d49
Masaaki Goshima <goccy54@gmail.com>
2013-11-22 01:13:05 +0900

    Checking in changes prior to tagging of version 0.08.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index c7c0e16..845501e 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension Compiler-Parser.
    
     {{$NEXT}}
    
    +0.08 2013-11-21T16:12:49Z
    +    - modify for Compiler::Lexer's update.
    +
     0.07 2013-09-29T09:55:43Z
         - support do statment
         - support three term operator

--- lib/Compiler/Parser.pm
+++ lib/Compiler/Parser.pm
@@ -38,7 +38,7 @@ our @ISA = qw(Exporter);
 our %EXPORT_TAGS = ( 'all' => [ qw() ] );
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw();
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 require XSLoader;
 XSLoader::load('Compiler::Parser', $VERSION);
 
